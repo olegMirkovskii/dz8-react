b77f16449cba9f56a379bff400eb43493ad27238
olegMirkovskii <121171060+olegMirkovskii@users.noreply.github.com>
2023-03-13 14:13:52 +0600

    Add files via upload

---
+++ lesson8/src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+function ParentComponent(props) {
+  const [childCount, setChildCount] = useState(0)
+
+  function addChilren() {
+    setChildCount(childCount + 1)
+  }
+
+  return (
+      <div className="parent">
+        <h1>{props.title}</h1>
+        <button onClick={addChilren}>Добавить абзац</button>
+        {[...Array(childCount)].map((_, index) =>(
+            <ChildComponent key={index} text={`номер ${index + 1}` } />
+        ))}
+      </div>
+  )
+}
+
+function ChildComponent({text}) {
+  return (
+      <p>{text}</p>
+  )
+}
+
+function App() {
+  return (
+      <ParentComponent title='Заголовок!' />
+
+  )
+}
+
+export default App
